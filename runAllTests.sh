3bbd601c52db91722b0c072ef0368b8278541b45
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-10-20 16:09:29 +0530

    Initial commit to draw patterns using string
    Draw Rectangle,Triangle and Diamond using string.
    Testing those patterns using run_test.sh Framework.

---
+++ runAllTests.sh
@@ -0,0 +1,6 @@
+#! /bin/bash 
+
+./runTest.sh ./src/rectanglePattern.js ./test/inputFiles/allInputsForRectangle ./test/outputFiles/allOutputsForRectangle
+./runTest.sh ./src/trianglePattern.js ./test/inputFiles/allInputsForTriangle ./test/outputFiles/allOutputsForTriangle
+./runTest.sh ./src/diamondPattern.js ./test/inputFiles/allInputsForDiamond ./test/outputFiles/allOutputsForDiamond
+
