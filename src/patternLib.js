e42621a28d8f57790117d4046017d3751e27b37a
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-11-19 18:29:16 +0530

    Refactor
    Long lines are  break into small parts for readability

--- src/patternLib.js
+++ src/patternLib.js
@@ -8,47 +8,36 @@ const {createFilledRect,
   createAngledDiamond } = require('./patternUtil.js');
 
 const generateRectangle = function(parameters) {
-  let rectangle = rectangleTypes[parameters.type](parameters.height,parameters.width);
+  let rectangleType = rectangleTypes[parameters.type];
+  let width = parameters.width;
+  let height = parameters.height;
+  let rectangle = rectangleType(height,width);
   return rectangle;
 }
 
 const generateTriangle = function(parameters) {
-  let triangle = triangleTypes[parameters.type](parameters.height);
+  let triangleType = triangleTypes[parameters.type];
+  let height = parameters.height;
+  let triangle = triangleType(height);
   return triangle;
-}
-
-const diamondTypes = {
-  "filled_diamond" : createFilledDiamond,
-  "hollow_diamond" : createHollowDiamond,
-  "angled_diamond" : createAngledDiamond,
-  "filled" : createFilledDiamond,
-  "hollow" : createHollowDiamond,
-  "angled" : createAngledDiamond
 }
 
 const generateDiamond = function (parameters) {
   if(parameters.height%2  == 0) {
     parameters.height --;
   }
-  let diamond = diamondTypes[parameters.type](parameters.height);
+  let diamondType = diamondTypes[parameters.type];
+  let height = parameters.height;
+  let diamond = diamondType(height);
   return diamond;
 }
-
-const typesOfPattern = {
-  "filled_rectangle" : generateRectangle,
-  "empty_rectangle" : generateRectangle,
-  "alternating_rectangle" : generateRectangle,
-  "left_triangle" : generateTriangle,
-  "right_triangle" : generateTriangle,
-  "filled_diamond" : generateDiamond,
-  "hollow_diamond" : generateDiamond,
-  "angled_diamond" : generateDiamond
-};
 
 const generatePattern = function(parameters) { 
   let patterns = [];
   for(let index=1; index<parameters.length; index++) {
-    patterns.push( typesOfPattern[parameters[index]["type"]](parameters[index]) );
+    let patternType = typesOfPattern[parameters[index]["type"]];
+    let line = patternType(parameters[index]);
+    patterns.push(line);
   }
   return patterns[0];
 }
@@ -69,6 +58,25 @@ const triangleTypes = {
   "right" : createRightTriangle,
 }
 
+const diamondTypes = {
+  "filled_diamond" : createFilledDiamond,
+  "hollow_diamond" : createHollowDiamond,
+  "angled_diamond" : createAngledDiamond,
+  "filled" : createFilledDiamond,
+  "hollow" : createHollowDiamond,
+  "angled" : createAngledDiamond
+}
+
+const typesOfPattern = {
+  "filled_rectangle" : generateRectangle,
+  "empty_rectangle" : generateRectangle,
+  "alternating_rectangle" : generateRectangle,
+  "left_triangle" : generateTriangle,
+  "right_triangle" : generateTriangle,
+  "filled_diamond" : generateDiamond,
+  "hollow_diamond" : generateDiamond,
+  "angled_diamond" : generateDiamond
+};
 
 exports.generatePattern = generatePattern;
 exports.generateDiamond = generateDiamond;
